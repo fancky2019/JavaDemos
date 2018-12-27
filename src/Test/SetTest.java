1db1bb86ad14a265f0fddaee84b18a8c43f43da5
fancky2019 <fancky2019@users.noreply.github.com>
2018-12-27 15:07:10 +0800

    集合

--- src/Test/SetTest.java
+++ src/Test/SetTest.java
@@ -1,5 +1,7 @@
 package Test;
 
+import Model.Student;
+
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -10,6 +12,7 @@ public class SetTest {
 
        // operation();
         sets();
+      //  getListObjectProperty();
     }
 
     private void operation() {
@@ -134,6 +137,65 @@ public class SetTest {
         }
         //endregion
 
+    }
+
+    private void getListObjectProperty() {
+        // Arrays.asList 返回的集合大小固定，无法添加和删除
+        List<Student> list = new ArrayList<>(Arrays.asList(new Student("fancky1", 1),
+                new Student("fancky2", 4),
+                new Student("fancky3", 2),
+                new Student("fancky4", 3)
+        ));
+
+        List<Student> list1 = new ArrayList<>(Arrays.asList(new Student("fancky5", 6),
+                new Student("fancky6", 5),
+                new Student("fancky7", 8),
+                new Student("fancky8", 7)
+        ));
+        //获取对象集合的某一属性集合
+        //lambda表达式
+        List<String> nameList = list.stream().map(p -> p.getName()).collect(Collectors.toList());
+        //方法引用::
+        List<String> nameList1 = list.stream().map(Student::getName).collect(Collectors.toList());
+        List<Integer> ageList = list.stream().map(p -> p.getAge()).collect(Collectors.toList());
+        //过滤
+        List<Student> filterList = list.stream().filter(p -> p.getAge() > 3).collect(Collectors.toList());
+//        //实现Comparable接口，然后重写compareTo方法。
+//        ageList.sort((a, b) ->
+//        {
+//            if (a >= b) {
+//                return 1;
+//            } else
+//
+//            {
+//                return -1;
+//            }
+//        });
+
+        //正序
+        Collections.sort(ageList);
+        //逆序
+        Collections.reverse(ageList);
+        //最大、最小
+        Integer maxAge = Collections.max(ageList);
+        Integer minAge = Collections.min(ageList);
+
+
+        //添加
+        list.add(new Student("fancky9", 10));
+        list.addAll(list1);
+
+
+        //添加
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("fancky9", 10));
+
+        List<Student> students1 = new ArrayList<>();
+        students.add(new Student("fancky11", 11));
+        students.add(new Student("fancky12", 12));
+
+        students.addAll(students1);
+        Integer m = 0;
     }
 
 }
