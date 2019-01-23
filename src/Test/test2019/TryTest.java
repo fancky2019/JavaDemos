0ce29a74da75e3f4805cd73e75278f3a76940d41
fancky <709737588@qq.com>
2019-01-23 15:41:34 +0800

    try

---
+++ src/Test/test2019/TryTest.java
@@ -0,0 +1,35 @@
+package Test.test2019;
+
+import java.io.*;
+
+public class TryTest {
+    public void test() {
+
+    }
+
+    private void tryFun() {
+        try {
+
+           //像C#嵌套using 一样
+            try (InputStream inputStream = new FileInputStream("")) {
+                try (OutputStream outputStream = new FileOutputStream("")) {
+
+                }
+            }
+
+            //多个语句写在一个try括号内，用分号隔开
+            try (InputStream inputStream = new FileInputStream("");
+                 OutputStream outputStream = new FileOutputStream("")) {
+
+                }
+
+        } catch (IOException io) {
+
+        } catch (Exception ex) {
+
+        } finally {
+
+        }
+    }
+
+}
