7702b162923f9d4a586142bc838e6c2d4d723129
fancky <709737588@qq.com>
2019-03-20 12:08:57 +0800

    maven 整理

---
+++ src/main/java/Test/opensource/redis/RedissonTest.java
@@ -0,0 +1,12 @@
+package Test.opensource.redis;
+
+/*
+Distributed locks with Redis:https://redis.io/topics/distlock
+Redisson github:https://github.com/redisson/redisson
+ */
+public class RedissonTest {
+    public  void test()
+    {
+
+    }
+}
