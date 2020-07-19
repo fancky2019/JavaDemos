d141945c6d57c2bf0335e93dbff31818da1effea
fancky <709737588@qq.com>
2020-07-19 18:11:50 +0800

    Redis订阅

--- src/main/java/Test/opensource/redis/RedisTest.java
+++ src/main/java/Test/opensource/redis/RedisTest.java
@@ -475,6 +475,46 @@ public class RedisTest {
     //endregion
 
     //region 订阅
+    private  void pubSub()
+    {
+        String pubSubChannel="";
+
+        CompletableFuture.runAsync(() ->
+        {
+            String  message="";
+            Long publish = jedis.publish(pubSubChannel, message);//返回订阅者数量
+
+
+        });
+
+
+        // region Consumer
+        CompletableFuture.runAsync(() ->
+        {
+
+            jedis.subscribe(new JedisPubSub()
+            {
+                @Override
+                /** JedisPubSub类是一个没有抽象方法的抽象类,里面方法都是一些空实现
+                 * 所以可以选择需要的方法覆盖,这儿使用的是SUBSCRIBE指令，所以覆盖了onMessage
+                 * 如果使用PSUBSCRIBE指令，则覆盖onPMessage方法
+                 * 当然也可以选择BinaryJedisPubSub,同样是抽象类，但方法参数为byte[]
+                 **/
+                public void onMessage(String channel, String message) {
+                    System.out.println(Thread.currentThread().getName()+"-接收到消息:channel=" + channel + ",message=" + message);
+                    //接收到exit消息后退出
+                    if (pubSubChannel.equals(message)) {
+                        System.exit(0);
+                    }
+                }
+
+            }, pubSubChannel);//第一个参数是处理接收消息，第二个参数是订阅的消息频道
+        });
+        //endregion
+
+    }
+
+    }
     //endregion
 
     //region 过期回调
