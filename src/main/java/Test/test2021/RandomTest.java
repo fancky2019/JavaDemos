a69557b0b89f5fbd4dc7c3d9970624ac10de912a
fancky2019 <709737588@qq.com>
2021-07-12 10:34:18 +0800

    添加注释1

--- src/main/java/Test/test2021/RandomTest.java
+++ src/main/java/Test/test2021/RandomTest.java
@@ -14,7 +14,7 @@ public class RandomTest {
         Random random = new Random(1);
         //(0,10000] 左开右闭
         int r1 = random.nextInt(10000);
-        //(0,10000]
+        //(0,10000] 左开右闭
         int r2 = random.nextInt(10000);
         int n = 0;
     }
