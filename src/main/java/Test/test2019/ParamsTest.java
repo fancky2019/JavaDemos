17355be527f65a8b6ff8b8962435568ac8c757ea
fancky <709737588@qq.com>
2022-06-09 11:30:25 +0800

    1

--- src/main/java/Test/test2019/ParamsTest.java
+++ src/main/java/Test/test2019/ParamsTest.java
@@ -2,6 +2,12 @@ package Test.test2019;
 
 /**
  * 变长参数
+ *
+ *
+ *      * 变长参数必须是最后一个参数，只能有一个变长参数：Vararg parameter must be the last in the list
+ *      * 不传参数，数组是个空数组，长度为0，但是不是null。
+ *      * 可以传单个元素，或者数组，或者逗号隔开的n个元素
+ *
  */
 public class ParamsTest {
     public void test() {
