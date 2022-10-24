1ef59ac54e4416c02d756944db0387bd1fac0a49
fancky2019 <709737588@qq.com>
2022-10-24 17:08:59 +0800

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -61,6 +61,7 @@ public class Main {
 //            System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
 
 //            1
+            //2
 
             //主线程设置捕获子线程的异常
             Thread.setDefaultUncaughtExceptionHandler((thread, throwable) ->
